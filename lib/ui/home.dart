ff2955c5280277a048dfb2a64b2477a7a41411f7
HarinKaklotar <harinlive@gmail.com>
2020-04-15 10:21:59 +0530

    use row widgets with expanded

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,26 +5,36 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-      color: Colors.deepOrangeAccent,
-      alignment: Alignment.center,
-      // height: 500,
-      // width: 300,
-      //margin: EdgeInsets.only(left: 50.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Pizza",
-            style: TextStyle(
-                color: Colors.white,
-                decoration: TextDecoration.none,
-                fontFamily: "Oxygen",
-                fontWeight: FontWeight.w700),
-            textDirection: TextDirection.ltr,
-          ),
-        ],
-      ),
-    )
-    );
+            color: Colors.deepOrangeAccent,
+            alignment: Alignment.center,
+            // height: 500,
+            // width: 300,
+            //margin: EdgeInsets.only(left: 50.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: Text(
+                  "Margrita",
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      fontFamily: "Oxygen",
+                      fontWeight: FontWeight.w700),
+                  textDirection: TextDirection.ltr,
+                )),
+                Expanded(
+                    child: Text(
+                  "Totamto, Mozarrela, Basil",
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      color: Colors.white,
+                      decoration: TextDecoration.none,
+                      fontFamily: "Oxygen",
+                      fontWeight: FontWeight.w700),
+                  textDirection: TextDirection.ltr,
+                ))
+              ],
+            )));
   }
 }
