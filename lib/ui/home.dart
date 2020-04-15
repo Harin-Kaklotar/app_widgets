ec33f69b8b3998e2281a18e023336b5940cb5ed6
HarinKaklotar <harinlive@gmail.com>
2020-04-15 10:28:26 +0530

    use column widgets and use padding

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -7,33 +7,64 @@ class Home extends StatelessWidget {
         child: Container(
             color: Colors.deepOrangeAccent,
             alignment: Alignment.center,
+            padding: EdgeInsets.only(left: 10.0, top: 30.0),
             // height: 500,
             // width: 300,
             //margin: EdgeInsets.only(left: 50.0),
-            child: Row(
+            child: Column(
               children: <Widget>[
-                Expanded(
-                    child: Text(
-                  "Margrita",
-                  style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      decoration: TextDecoration.none,
-                      fontFamily: "Oxygen",
-                      fontWeight: FontWeight.w700),
-                  textDirection: TextDirection.ltr,
-                )),
-                Expanded(
-                    child: Text(
-                  "Totamto, Mozarrela, Basil",
-                  style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.white,
-                      decoration: TextDecoration.none,
-                      fontFamily: "Oxygen",
-                      fontWeight: FontWeight.w700),
-                  textDirection: TextDirection.ltr,
-                ))
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Margrita",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                          fontFamily: "Oxygen",
+                          fontWeight: FontWeight.w700),
+                      textDirection: TextDirection.ltr,
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Totamto, Mozarrela, Basil",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                          fontFamily: "Oxygen",
+                          fontWeight: FontWeight.w700),
+                      textDirection: TextDirection.ltr,
+                    ))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Marinara",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                          fontFamily: "Oxygen",
+                          fontWeight: FontWeight.w700),
+                      textDirection: TextDirection.ltr,
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Totamto, Garlic",
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                          fontFamily: "Oxygen",
+                          fontWeight: FontWeight.w700),
+                      textDirection: TextDirection.ltr,
+                    ))
+                  ],
+                )
               ],
             )));
   }
